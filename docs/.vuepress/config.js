1080829b0f2677332eee00c73dff7ec37c452b5c
L·Lawliet <553867163@qq.com>
2018-07-29 20:21:27 +0800

    船新改版 支持离线阅读

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  base: '/hzfe-questions-and-answers/',
+  title: 'HZFE奶库',
+  description: '前端面试题库',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
+  serviceWorker: true,
+  themeConfig: {
+    repo: 'HZFE/hzfe-questions-and-answers',
+    repoLabel: 'GITHUB',
+    docsDir: 'docs',
+    editLinks: true,
+    editLinkText: '在 GitHub 上编辑此页',
+    nav: [
+      { text: 'HZFE', link: 'https://hzfe.org' }
+    ],
+    sidebar: 'auto',
+    lastUpdated: '上次更新'
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: /logo.png
+actionText: 开始学习 →
+actionLink: /toc
+features:
+- title: 快
+  details: 大厂面试精华
+- title: 去
+  details: 成员亲自上阵
+- title: 学
+  details: 支持离线阅读
+footer: MIT Licensed | Copyright © 2016-present HZFE
+---
